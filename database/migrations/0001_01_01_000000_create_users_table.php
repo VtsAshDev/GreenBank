2ec0bdbc175e52b128e2ac6f2a12683e5ed38e17
VitorVts <vitorgguimaraes56@gmail.com>
2025-05-26 10:57:54 -0300

    feat(database): Cria migration para a tabela 'transaction'

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('document');
-            $table->double('balance');
             $table->boolean('shopkeeper');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
